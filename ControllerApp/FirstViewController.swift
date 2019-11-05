4fa28518e9573b7620bc79c7cb88a1b0207eb010
Karlis Cars <karlis.cars@gmail.com>
2019-11-05 02:30:18 +0200

    Made TabBar and NavController. Still have to tinker around Nav and implement Dark mode. Dark mode question. If i make a Dark mode function in one of .swift which should be main one and how i could use it in other view controllers? Maybe classes and inherit this func?

---
+++ ControllerApp/FirstViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FirstViewController.swift
+//  ControllerApp
+//
+//  Created by Karlis Cars on 04/11/2019.
+//  Copyright © 2019 Karlis Cars. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ControllerApp/Firstview.swift
@@ -0,0 +1,19 @@
+//
+//  Firstview.swift
+//  ControllerApp
+//
+//  Created by Karlis Cars on 04/11/2019.
+//  Copyright © 2019 Karlis Cars. All rights reserved.
+//
+
+import UIKit
+
+class Firstview: UIViewController {
+
+override func viewDidLoad() {
+    super.viewDidLoad()
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        <#code#>
+    }
+}
